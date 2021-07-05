50f09717ca25135e640049aa30bf226a5177217c
Md. Riad Hassan <riad_hassan@outlook.com>
2021-07-05 23:28:23 +0600

    Update settings.py

--- event_registration/settings.py
+++ event_registration/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3ute5m04d#54c$43(s$bg7#od*-04ow86p+u)8#)j)cw%jn#qe'
+SECRET_KEY = 'Your_Secret_Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
